ed78d78cb16b9d85b7189192fc65621fc025a6cb
Ella Marie A. Mallari <elleee.mallari@gmail.com>
2023-05-10 14:06:01 +0800

    Import tkinter

--- SimpleAppCalculator.py
+++ SimpleAppCalculator.py
@@ -1,2 +1,16 @@
 # ELLA MARIE A. MALLARI
 # BSCPE 1-4 
+
+# CALCULATOR APP
+
+#import tkinter to make the calculator visible (GUI/UI)
+import tkinter as tk 
+
+root = tk.Tk()
+root.title("Calculator")
+root.geometry("345x460")
+root.resizable(0,0)
+root.configure(bg="black")
+
+
+root.mainloop()
