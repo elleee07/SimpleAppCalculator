30506e8445e1e1a8954f38f5b2e86d06e4f8429b
Ella Marie A. Mallari <elleee.mallari@gmail.com>
2023-05-10 18:36:13 +0800

    Trying Exception Handling

--- SimpleAppCalculator.py
+++ SimpleAppCalculator.py
@@ -3,6 +3,26 @@
 
 # CALCULATOR APP
 
+# handling unchecked exception 
+try: 
+    def button_click(number):
+        current = input_box.get()
+        input_box.delete(0, tk.END)
+        input_box.insert(0, str(current) + str(number))
+except TypeError:
+    print("ERRORRRR")
+except IndentationError:
+    print("Indentation error found.")
+
+except:
+    print("Error alert.")
+
+finally:
+    print("No error found.")
+
+
+
+
 #import tkinter to make the calculator visible (GUI/UI)
 import tkinter as tk 
 
@@ -12,5 +32,5 @@ root.geometry("345x460")
 root.resizable(0,0)
 root.configure(bg="black")
 
-
 root.mainloop()
+
